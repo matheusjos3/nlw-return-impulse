c11df61fe03effcd5cfb26586de0de52f25181d2
Matheus <matheusdc98@gmail.com>
2022-05-08 13:22:15 -0300

    first commit

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from 'nodemailer'
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: "cec8af732af28f",
+        pass: "54b8c8c8253172"
+    }
+});
+
+export class NodeMailerMailAdapter implements MailAdapter {
+    async sendMail({ subject, body }: SendMailData) {
+        await transport.sendMail({
+            from: 'Equipe Feedget <oi@feedget.com>',
+            to: 'Matheus José <mattheusjose55@gmail.com',
+            subject,
+            html: body
+        })
+    }
+}
---
+++ server/src/use-cases/submit-feedback-use-case.test.ts
@@ -0,0 +1,30 @@
+import { SubmitFeedbackUseCase } from "./submit-feedback-use-case"
+
+const createFeedbackSpy = jest.fn()
+const sendMailSpy = jest.fn()
+
+const submitFeedback = new SubmitFeedbackUseCase(
+    { create: createFeedbackSpy },
+    { sendMail: sendMailSpy }
+)
+
+describe('Submit feedback', () => {
+    it('should be able to submit a feedback', async () => {
+        await expect(submitFeedback.execute({
+            type: 'BUG',
+            comment: 'example comment',
+            screenshot: 'data:image/png;base64,fdsfdsfsdfsdfsf'
+        })).resolves.not.toThrow();
+
+        expect(createFeedbackSpy).toHaveBeenCalled()
+        expect(sendMailSpy).toHaveBeenCalled()
+    })
+
+    it('should not be able to submit a feedback without type', async () => {
+        await expect(submitFeedback.execute({
+            type: '',
+            comment: 'example comment',
+            screenshot: 'data:image/png;base64,fdsfdsfsdfsdfsf'
+        })).rejects.toThrow();
+    })
+})
